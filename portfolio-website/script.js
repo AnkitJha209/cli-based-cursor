24b72526e44d61f0bbd8a531a332089b5bb46112
Ankit Jha <professionalaj209@gmail.com>
2025-06-09 02:27:58 +0530

    cli-based-cursor

---
+++ portfolio-website/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Add any JS code here that needs to run after the document is loaded.
+    console.log('Portfolio website JS loaded!');
+});
